0d94970f57100fd0d092743be6cd892d48e935ba
sandushi <sandushidileka2@gmail.com>
2021-06-04 18:14:53 +0530

    php varibles

---
+++ basic/variables.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My Variables PHP page</h1>
+
+<?php
+    //String variable
+    $name = "Sandushi Dileka";
+    $age = 25;
+    $university = "University of Peradeniya";
+
+    echo "My name is $name and I am $age years old. I graduated from $university";
+?>
+
+</body>
+</html>
